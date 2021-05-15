09a5ec179c2b4e54e09a216391bd539ea551f033
Hyuga-Tsukui <gakikiki407@gmail.com>
2021-05-15 22:54:34 +0900

    map

---
+++ index.js
@@ -0,0 +1,12 @@
+const map = new Map()
+
+map.set('Jessie', {phone: "213-555-1234", address: "123 N 1st Ave"})
+
+const dict = {}
+dict['a'] = 'a'
+
+console.log(map)
+console.log(dict)
+
+console.log(typeof(map))
+console.log(typeof(dict))
